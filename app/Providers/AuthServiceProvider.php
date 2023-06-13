ff73ed3be01564ea680817c2fd75c7104c06f955
JunarisAlf <junaris0715@gmail.com>
2023-06-13 16:33:46 +0700

    ADD SuperAdmin and Admin Gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,15 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('superadmin', function(User $user){
+            if($user->role == 'superadmin'){
+                return true;
+            }
+        });
+        Gate::define('admin', function(User $user){
+            if($user->role == 'admin'){
+                return true;
+            }
+        });
     }
 }
